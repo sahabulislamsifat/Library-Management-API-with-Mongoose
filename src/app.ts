4133578a312eff00d9f900b171751f2c857c99b5
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-06-25 23:21:03 +0600

    route path change

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(requestLogger);
 
 // API Routes
-app.use("/", routes);
+app.use("/api", routes);
 
 // Health Check Route
 app.get("/", (req: Request, res: Response) => {
